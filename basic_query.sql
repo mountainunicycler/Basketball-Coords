ad7f889b6a042e89813e7d9578b81526798b2d96
Ryan Zimmerman <ryan@Ryans-MacBook-Pro.local>
2019-05-01 20:20:53 -0500

    added basic sql query

---
+++ basic_query.sql
@@ -0,0 +1,7 @@
+SELECT home_name, away_name, event_coord_x, event_coord_y, shot_type, shot_subtype, three_point_shot, points_scored
+FROM `bigquery-public-data.ncaa_basketball.mbb_pbp_sr`
+WHERE home_name = 'Bears' 
+AND shot_type IS NOT NULL
+AND points_scored is NOT NULL
+AND event_coord_x IS NOT NULL
+AND event_coord_y IS NOT NULL
