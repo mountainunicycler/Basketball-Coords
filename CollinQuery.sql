209eae59f25bda439cfdd247c00495207ac40d9e
Ryan Zimmerman <ryan@Ryans-MacBook-Pro.local>
2019-05-07 10:33:21 -0500

    added New query

---
+++ CollinQuery.sql
@@ -0,0 +1,4 @@
+ SELECT home_name, away_name, event_coord_x, event_coord_y, shot_type, shot_subtype, three_point_shot, points_scored
+ FROM `bigquery-public-data.ncaa_basketball.mbb_pbp_sr`
+
+ WHERE
