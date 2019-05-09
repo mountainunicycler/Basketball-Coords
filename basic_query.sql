8071b40c81223547357126f1ef9de1b238d0b37f
Collin Nill <nill1@r1.stolaf.edu>
2019-05-09 10:29:17 -0500

    Added team code

--- basic_query.sql
+++ basic_query.sql
@@ -5,3 +5,12 @@ AND shot_type IS NOT NULL
 AND points_scored is NOT NULL
 AND event_coord_x IS NOT NULL
 AND event_coord_y IS NOT NULL
+
+
+-------------
+
+
+SELECT team_name, three_point_shot, shot_made, type, shot_subtype, points_scored
+FROM `bigquery-public-data.ncaa_basketball.mbb_pbp_sr`
+WHERE team_name = 'Cavaliers' 
+AND type IN ('freethrow', 'fieldgoal')
